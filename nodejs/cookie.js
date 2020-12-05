03aa6d6cc4893e0ba5a690fceaf254975079f1ca
dkstjdwns114 <sungjoon256@gmail.com>
2020-12-05 16:09:04 +0900

    쿠키의 읽기

--- nodejs/cookie.js
+++ nodejs/cookie.js
@@ -1,9 +1,16 @@
 let http = require("http");
+let cookie = require("cookie");
 http
   .createServer(function (request, response) {
-    // response.writeHead(200, {
-    //   "Set-Cookie": ["yummy_cookie=choco", "tasty_cookie=strawberry"]
-    // });
+    console.log(request.headers.cookie);
+    let cookies = {};
+    if (request.headers.cookie !== undefined) {
+      cookies = cookie.parse(request.headers.cookie);
+    }
+    console.log(cookies.yummy_cookie);
+    response.writeHead(200, {
+      "Set-Cookie": ["yummy_cookie=choco", "tasty_cookie=strawberry"]
+    });
     response.end("Cookie!!");
   })
   .listen(3000);
